49aa680ca644b8a3ad71fab4f76104bdd2fb1d0e
Paul <webmoney91rus@yandex.ru>
2019-05-19 21:02:51 +0300

    added select of brand and model

--- js/app.js
+++ js/app.js
@@ -40,3 +40,52 @@ app.getDataFromDB();
 	result = JSON.parse(result);
 	app.brands = result;
 });*/
+
+var nav = new Vue({
+	el:'#nav',
+	data: {
+		currentBrand: null,
+		currentModel: null,
+		currentEngine: null,
+		brands: null,
+		models: null,
+		engines: null
+	},
+	methods: {
+		getBrands: function() {
+			// alert('../php/main.php?entity=brand');
+			axios.get('../php/main.php?entity=brand').then(function(response) {
+				var result = JSON.stringify(response.data);
+				result = JSON.parse(result);
+				/*if (result != null)
+					nav.currentBrand = result[0].id;*/
+				nav.brands = result;
+				// alert("end of get Brands " + this.brands);
+				nav.getModels();
+			})
+		},
+		getModels: function() {
+			// alert(this.currentBrand);
+			// alert("getModels()");
+			// alert('../php/main.php?fun=findById&entity=model&id='+this.currentBrand+'&relatedEntity=brand');
+			axios.get('../php/main.php?fun=findById&entity=model&id='+this.currentBrand+'&relatedEntity=brand').then(function(response) {
+				var result = JSON.stringify(response.data);
+				result = JSON.parse(result);
+				currentModel: result[0].id;
+				nav.models = result;
+			})
+		},
+		changeBrand: function(event) {
+			// alert("brand changed");
+			this.currentBrand = event.target.value;
+			this.getModels();
+		}
+	}
+});
+
+nav.getBrands();
+/*axios.get('../php/main.php?entity=brand').then(function(response) {
+				var result = JSON.stringify(response.data);
+				result = JSON.parse(result);
+				nav.brands = result;
+			});*/
--- php/main.php
+++ php/main.php
@@ -5,13 +5,37 @@ require_once 'DBHelper.php';
 
 header('Content-Type: application/json');
 $db = new DBHelper($host, $user, $password, $database);
-//$db->printVars();
-//echo $_SERVER['REQUEST_METHOD'];
+
 if (!empty($_GET) && count($_GET) == 1 && !empty($_GET['entity'])) {
+    // echo "0";
 	$result = $db->getQuery("select * from ".$_GET['entity']);
 	if (!empty($result)) {
 		echo json_encode($result);
 	}
+} elseif(!empty($_GET['fun'])) {
+    // echo "1";
+    // echo ($_GET['fun']);
+    switch($_GET['fun']) {
+        case "findById":
+        // echo "3"; 
+            if (!empty($_GET['id']) && !empty($_GET['entity'])) {
+                // echo "4";
+                if (!empty($_GET['relatedEntity'])){
+                    // echo "5";
+                    // echo "\n";
+                    // echo "select * from ".$_GET['entity']." where ".$_GET['relatedEntity']."_id == ".$_GET[id];
+                    // echo "\n";
+                    $result = $db -> getQuery("select * from ".$_GET['entity']." where ".$_GET['relatedEntity']."_id = ".$_GET[id]);
+                } 
+                else {
+                    // echo "6";
+                    $result = $db -> getQuery("select * from ".$_GET['entity']." where id = ".$_GET[id]);
+                }
+                // echo "7";
+                if (!empty($result)) {
+                    // echo "8";
+                    echo json_encode($result);
+                }
+            }
+    }
 }
-
-	
