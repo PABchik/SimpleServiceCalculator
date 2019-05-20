5c3dc9b0323a4e27f03e5a5a1c0eea34201d6ae6
Paul <webmoney91rus@yandex.ru>
2019-05-20 12:20:31 +0300

    Update db

--- services.sql
+++ services.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3307
--- Время создания: Май 20 2019 г., 02:41
+-- Время создания: Май 20 2019 г., 12:19
 -- Версия сервера: 5.6.38
 -- Версия PHP: 5.5.38
 
@@ -113,6 +113,68 @@ INSERT INTO `model` (`id`, `name`, `brand_id`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `part`
+--
+
+CREATE TABLE `part` (
+  `code` varchar(150) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `price` int(11) NOT NULL,
+  `part_type_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `part`
+--
+
+INSERT INTO `part` (`code`, `name`, `price`, `part_type_id`) VALUES
+('engineoil1', 'двигательное масло bmw 1k', 3000, 1),
+('engineoil2', 'двигательное масло bmw 3k', 5000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `part_type`
+--
+
+CREATE TABLE `part_type` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `part_type`
+--
+
+INSERT INTO `part_type` (`id`, `name`) VALUES
+(1, 'двигательное масло'),
+(2, 'масляный фильтр'),
+(3, 'масло для КП');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `part_type_for_service`
+--
+
+CREATE TABLE `part_type_for_service` (
+  `id` int(11) NOT NULL,
+  `part_type_id` int(11) NOT NULL,
+  `service_for_car_id` int(11) NOT NULL,
+  `count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `part_type_for_service`
+--
+
+INSERT INTO `part_type_for_service` (`id`, `part_type_id`, `service_for_car_id`, `count`) VALUES
+(1, 1, 1, 3),
+(2, 2, 1, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `service`
 --
 
@@ -186,6 +248,27 @@ ALTER TABLE `model`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `part`
+--
+ALTER TABLE `part`
+  ADD PRIMARY KEY (`code`),
+  ADD KEY `part_type_id` (`part_type_id`);
+
+--
+-- Индексы таблицы `part_type`
+--
+ALTER TABLE `part_type`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `part_type_for_service`
+--
+ALTER TABLE `part_type_for_service`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `service_for_car_id` (`service_for_car_id`),
+  ADD KEY `part_type_id` (`part_type_id`);
+
+--
 -- Индексы таблицы `service`
 --
 ALTER TABLE `service`
@@ -211,6 +294,19 @@ ALTER TABLE `service_for_car`
 ALTER TABLE `engine_model`
   ADD CONSTRAINT `engine_model_ibfk_1` FOREIGN KEY (`engine_id`) REFERENCES `engine` (`code`),
   ADD CONSTRAINT `engine_model_ibfk_2` FOREIGN KEY (`model_id`) REFERENCES `model` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `part`
+--
+ALTER TABLE `part`
+  ADD CONSTRAINT `part_ibfk_1` FOREIGN KEY (`part_type_id`) REFERENCES `part_type` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `part_type_for_service`
+--
+ALTER TABLE `part_type_for_service`
+  ADD CONSTRAINT `part_type_for_service_ibfk_1` FOREIGN KEY (`service_for_car_id`) REFERENCES `service_for_car` (`id`),
+  ADD CONSTRAINT `part_type_for_service_ibfk_2` FOREIGN KEY (`part_type_id`) REFERENCES `part_type` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `service_for_car`
